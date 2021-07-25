be55fcbe89f886d946f9bd94c9e2303cdedab38a
alazogon <84949610+alazogon@users.noreply.github.com>
2021-07-24 22:16:45 -0400

    Update stress-off.py

--- stress-off.py
+++ stress-off.py
@@ -1,1 +1,8 @@
-#Start of the Stress-Off program
+# Start of the Stress-Off program
+# Created by Angel Lazo-Gonzalez 
+
+# User Greeting
+print("Welcome to Stress-Off! This program will ask you a series of questions regarding your current feelings and symptoms.)
+print("In answering these questions, this program will inform you of remedies and solutions in regards to reducing and helping out your stress.")
+      
+# Stress symptoms explained
