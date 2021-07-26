b79f7f64ff482eb31a68c9850fb4843a568cb334
alazogon <84949610+alazogon@users.noreply.github.com>
2021-07-25 22:43:03 -0400

    Update stress-off.py
    Adding more code to the py file. Will be going through a majority of the next symptoms hopefully in my next commit.

--- stress-off.py
+++ stress-off.py
@@ -11,3 +11,28 @@ webmd_symptoms = ["Becoming easily agitated, feeling overwhelmed, avoiding other
 stress_symptoms =  ", ".join (webmd_symptoms)
 #Inform user of the most common symptoms found on Webmd
 print("Based on the research from Webmb, the current most common symptons of stress are:", stress_symptoms)
+
+#Establish repository for user responses
+user_input = []
+user_symptoms = []
+
+#Program is about to inform user on how to insert their information
+print("Hope everything is going swell so far, we are about to begin the questions included in our program today. Please type '1' for 'Yes' or '0' for 'No' for your responses.")
+#Begin asking questions starting with symptom 1
+symptom1 = input("Within the last few days or hours, have you noticed yourself getting easily agitated with others? ")
+while True:
+      if symptom1 == "1":
+          user_input += ("1")
+          user_symptoms.append("Becoming easily agitated")
+          break
+      elif symptom1 == "0":
+          break
+      else:
+          if symptom1 != "1" or "0":
+               print("Input is incorrect, please remember to enter '1' for 'Yes' or '0' for 'No'.")
+               symptom1 = input("Within the last few days or hours, have you noticed yourself getting easily agitated with others? ")
+#End symptom 1
+
+#Begin questions for symptom 2
+ 
+     
