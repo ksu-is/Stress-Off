881982a365421943589bcf2c3ed2eb0a20f5c4aa
alazogon <84949610+alazogon@users.noreply.github.com>
2021-07-26 14:49:34 -0400

    Update stress-off.py
    Code was running incorrectly. Fixed missing "

--- stress-off.py
+++ stress-off.py
@@ -2,7 +2,7 @@
 # Created by Angel Lazo-Gonzalez 
 
 # User Greeting
-print("Welcome to Stress-Off! This program will ask you a series of questions regarding your current feelings and symptoms.)
+print("Welcome to Stress-Off! This program will ask you a series of questions regarding your current feelings and symptoms.")
 print("In answering these questions, this program will inform you of remedies and solutions in regards to reducing and helping out your stress.")
 print("The goal in going through this program is to hopefully release some tension and get some assistance to help you in your current situation, I hope you enjoy the program and get what you were hoping out of it!")
      
